26717cc6f38be44486ca7753b215c1998e757fc5
LenaSpivakova <Skarlet2710@mail.ru>
2025-06-06 01:00:42 +0300

    add Corrections

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -2,14 +2,13 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDAO;
 import web.model.User;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 public class UserServiceImpl implements UserService {
     private final UserDAO userDao;
 
@@ -19,21 +18,25 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void saveUser(User user) {
         userDao.save(user);
     }
 
     @Override
+    @Transactional
     public void deleteUser(long id) {
         userDao.delete(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getUser(long id) {
         return userDao.get(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> getAllUsers() {
         return userDao.getAll();
     }
